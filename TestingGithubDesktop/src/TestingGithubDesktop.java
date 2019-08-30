c0071c2d371ee59ca10a6f149447fe14f19538c5
fdgifford3 <fdgifford3@gmail.com>
2019-08-30 16:41:38 -0500

    Testing a commit

--- TestingGithubDesktop/src/TestingGithubDesktop.java
+++ TestingGithubDesktop/src/TestingGithubDesktop.java
@@ -3,7 +3,10 @@ public class TestingGithubDesktop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String gitVar;
+		gitVar = "Testing Branching";
         System.out.println("Hello Github");
+        System.out.println(gitVar);
 	}
 
 }
